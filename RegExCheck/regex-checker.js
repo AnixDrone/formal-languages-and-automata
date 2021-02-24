4f2f24df8f27f3d3dca037345c6f7337a60becf9
martinkozle <martinkozle@yahoo.com>
2021-02-24 08:23:34 +0100

    Moved SVG strings to Icons class

--- RegExCheck/regex-checker.js
+++ RegExCheck/regex-checker.js
@@ -1,7 +1,21 @@
+var Icons = /** @class */ (function () {
+    function Icons() {
+    }
+    Icons.successSVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-check-square\" viewBox=\"0 0 16 16\">\n" +
+        "  <path d=\"M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z\"/>\n" +
+        "  <path d=\"M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z\"/>\n" +
+        "</svg>";
+    Icons.failureSVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"gray\" class=\"bi bi-x-square-fill\" viewBox=\"0 0 16 16\">\n" +
+        "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z\"/>\n" +
+        "</svg>";
+    Icons.trashSVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"red\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">\n" +
+        "  <path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/>\n" +
+        "  <path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/>\n" +
+        "</svg>";
+    return Icons;
+}());
 function checkForAllWords(pattern) {
-    // let trueWords = document.getElementById("trueWords").getElementsByClassName("row");
     var trueWords = document.querySelectorAll("#trueWords > div");
-    //let falseWords = document.getElementById("falseWords").getElementsByTagName("div");
     var falseWords = document.querySelectorAll("#falseWords > div");
     for (var i = 0; i < trueWords.length; i++) {
         var divs = trueWords.item(i).getElementsByTagName('div');
@@ -11,13 +25,10 @@ function checkForAllWords(pattern) {
         if (checkWord(pattern, word.textContent)) {
             success.setAttribute("style", "fill:green");
             failure.setAttribute("style", "fill:gray");
-            // success.style.fill="green";
-            // word.style.backgroundColor = "green";
         }
         else {
             success.setAttribute("style", "fill:gray");
             failure.setAttribute("style", "fill:red");
-            // word.style.backgroundColor = "red";
         }
     }
     for (var i = 0; i < falseWords.length; i++) {
@@ -58,22 +69,15 @@ function addWord(inputWord, ulId) {
         par.className = "col-sm-6";
         var success = document.createElement("div");
         success.className = "col-sm-1";
-        success.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"gray\" class=\"bi bi-check-square-fill\" viewBox=\"0 0 16 16\">\n" +
-            "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z\"/>\n" +
-            "</svg>";
+        success.innerHTML = Icons.successSVG;
         var failure = document.createElement("div");
         failure.className = "col-sm-1";
-        failure.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"gray\" class=\"bi bi-x-square-fill\" viewBox=\"0 0 16 16\">\n" +
-            "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z\"/>\n" +
-            "</svg>";
+        failure.innerHTML = Icons.failureSVG;
         row.appendChild(par);
         row.appendChild(success);
         row.appendChild(failure);
         var del = document.createElement("button");
-        del.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"red\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">\n" +
-            "  <path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/>\n" +
-            "  <path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/>\n" +
-            "</svg>";
+        del.innerHTML = Icons.trashSVG;
         del.className = "btn btn-sm offset-3 col-sm-1";
         del.onclick = function () {
             element.removeChild(row);
--- RegExCheck/regex-checker.ts
+++ RegExCheck/regex-checker.ts
@@ -1,3 +1,18 @@
+class Icons {
+    static successSVG: string = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-check-square\" viewBox=\"0 0 16 16\">\n" +
+        "  <path d=\"M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z\"/>\n" +
+        "  <path d=\"M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z\"/>\n" +
+        "</svg>"
+    static failureSVG: string = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"gray\" class=\"bi bi-x-square-fill\" viewBox=\"0 0 16 16\">\n" +
+        "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z\"/>\n" +
+        "</svg>";
+    static trashSVG: string = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"red\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">\n" +
+        "  <path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/>\n" +
+        "  <path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/>\n" +
+        "</svg>";
+}
+
+
 function checkForAllWords(pattern: string) {
     let trueWords = document.querySelectorAll("#trueWords > div");
     let falseWords = document.querySelectorAll("#falseWords > div");
@@ -8,11 +23,11 @@ function checkForAllWords(pattern: string) {
         let failure = divs.item(2).querySelector("svg");
 
         if (checkWord(pattern, word.textContent)) {
-            success.setAttribute("style","fill:green");
-            failure.setAttribute("style","fill:gray");
+            success.setAttribute("style", "fill:green");
+            failure.setAttribute("style", "fill:gray");
         } else {
-            success.setAttribute("style","fill:gray");
-            failure.setAttribute("style","fill:red");
+            success.setAttribute("style", "fill:gray");
+            failure.setAttribute("style", "fill:red");
 
         }
     }
@@ -22,11 +37,11 @@ function checkForAllWords(pattern: string) {
         let success = divs.item(1).querySelector("svg");
         let failure = divs.item(2).querySelector("svg");
         if (!checkWord(pattern, word.textContent)) {
-            success.setAttribute("style","fill:green");
-            failure.setAttribute("style","fill:gray");
+            success.setAttribute("style", "fill:green");
+            failure.setAttribute("style", "fill:gray");
         } else {
-            success.setAttribute("style","fill:gray");
-            failure.setAttribute("style","fill:red");
+            success.setAttribute("style", "fill:gray");
+            failure.setAttribute("style", "fill:red");
         }
     }
 }
@@ -50,31 +65,22 @@ function addWord(inputWord, ulId) {
     for (let word of words) {
         let element = document.getElementById(ulId);
         let row = document.createElement("div");
-        row.className="row";
-        let par=document.createElement("div");
+        row.className = "row";
+        let par = document.createElement("div");
         par.appendChild(document.createTextNode(word));
-        par.className="col-sm-6";
+        par.className = "col-sm-6";
 
-        let success=document.createElement("div");
-        success.className="col-sm-1";
-        success.innerHTML="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"gray\" class=\"bi bi-check-square-fill\" viewBox=\"0 0 16 16\">\n" +
-            "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z\"/>\n" +
-            "</svg>";
-        let failure=document.createElement("div");
-        failure.className="col-sm-1";
-        failure.innerHTML="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"gray\" class=\"bi bi-x-square-fill\" viewBox=\"0 0 16 16\">\n" +
-            "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z\"/>\n" +
-            "</svg>";
-
-
+        let success = document.createElement("div");
+        success.className = "col-sm-1";
+        success.innerHTML = Icons.successSVG;
+        let failure = document.createElement("div");
+        failure.className = "col-sm-1";
+        failure.innerHTML = Icons.failureSVG;
         row.appendChild(par);
         row.appendChild(success);
         row.appendChild(failure);
         let del = document.createElement("button");
-        del.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"red\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">\n" +
-            "  <path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/>\n" +
-            "  <path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/>\n" +
-            "</svg>";
+        del.innerHTML = Icons.trashSVG;
         del.className = "btn btn-sm offset-3 col-sm-1";
         del.onclick = function () {
             element.removeChild(row);
