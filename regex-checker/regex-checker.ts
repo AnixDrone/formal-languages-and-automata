7dd288189a42918374d8eca4850a1b44f44b5c8a
AnixDrone <tomislavignjatov@rocketmail.com>
2021-02-24 11:25:46 +0100

    fixed success icon

--- regex-checker/regex-checker.ts
+++ regex-checker/regex-checker.ts
@@ -1,8 +1,7 @@
 class Icons {
-    static successSVG: string = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-check-square\" viewBox=\"0 0 16 16\">\n" +
-        "  <path d=\"M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z\"/>\n" +
-        "  <path d=\"M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z\"/>\n" +
-        "</svg>"
+    static successSVG: string = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-check-square-fill\" viewBox=\"0 0 16 16\">\n" +
+        "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z\"/>\n" +
+        "</svg>";
     static failureSVG: string = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"gray\" class=\"bi bi-x-square-fill\" viewBox=\"0 0 16 16\">\n" +
         "  <path d=\"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z\"/>\n" +
         "</svg>";
@@ -52,7 +51,6 @@ function checkWord(pattern: string, word: string): boolean {
         let re = new RegExp("^" + pattern + "$");
         result = re.test(word);
     } catch (err) {
-
         result = false;
     }
     return result;
@@ -66,6 +64,7 @@ function addWord(inputWord, ulId) {
         let element = document.getElementById(ulId);
         let row = document.createElement("div");
         row.className = "row";
+
         let par = document.createElement("div");
         par.appendChild(document.createTextNode(word));
         par.className = "col-sm-6";
@@ -73,18 +72,21 @@ function addWord(inputWord, ulId) {
         let success = document.createElement("div");
         success.className = "col-sm-1";
         success.innerHTML = Icons.successSVG;
+
         let failure = document.createElement("div");
         failure.className = "col-sm-1";
         failure.innerHTML = Icons.failureSVG;
-        row.appendChild(par);
-        row.appendChild(success);
-        row.appendChild(failure);
+
         let del = document.createElement("button");
         del.innerHTML = Icons.trashSVG;
         del.className = "btn btn-sm offset-3 col-sm-1";
         del.onclick = function () {
             element.removeChild(row);
         };
+
+        row.appendChild(par);
+        row.appendChild(success);
+        row.appendChild(failure);
         row.appendChild(del);
         element.appendChild(row);
     }
